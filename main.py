a8d58c3c66a8e703c2384e37badef4a74ba40e9d
rick1177 <gureev.prof@gmail.com>
2022-01-22 15:20:12 +0500

    Создание классов

--- main.py
+++ main.py
@@ -3,14 +3,29 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+class Myclass_1:
+    #x: int
+    def __init__(self,x=0):
+        self.x = x
+
+class Myclass_2:
+    x: int
+    def __init__(self):
+        pass
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    #print('Hi,', name)  # Press Ctrl+F8 to toggle the breakpoint.
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    MyObject_1 = Myclass_1()
+    MyObject_1.x = 10.5
+    print(MyObject_1.x)
+    MyObject_2 = Myclass_2()
+    MyObject_2.x = 10.5
+    print (MyObject_2.x)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
